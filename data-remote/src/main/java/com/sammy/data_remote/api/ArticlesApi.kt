33505ee70ba14c55a072632b67a5ffdb0bf624ff
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-30 19:35:59 +0300

    modify article repository

--- data-remote/src/main/java/com/sammy/data_remote/api/ArticlesApi.kt
+++ data-remote/src/main/java/com/sammy/data_remote/api/ArticlesApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface ArticlesApi {
     @GET("search")
-    fun getDna(
+    fun getArticles(
         @Query("q") title: String
-    ): Flow<ArticleData>
+    ): ArticleData
 }
--- data-remote/src/main/java/com/sammy/data_remote/repository/ArticleRepository.kt
+++ data-remote/src/main/java/com/sammy/data_remote/repository/ArticleRepository.kt
@@ -1,4 +1,20 @@
 package com.sammy.data_remote.repository
 
-class ArticleRepository {
+import com.sammy.data_remote.api.ArticlesApi
+import com.sammy.domain.model.DocDomain
+import com.sammy.domain.repository.IArticlesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class ArticleRepository(
+    private val articlesApi: ArticlesApi
+):IArticlesRepository {
+    override suspend fun getCurrentLocation(search: String): Flow<List<DocDomain>> = flow {
+        val result = articlesApi.getArticles(title = search)
+        val articleList = mutableListOf<DocDomain>()
+        result.response.forEach { article ->
+           articleList.add(articleList)
+        }
+        emit(articleList)
+    }
 }
