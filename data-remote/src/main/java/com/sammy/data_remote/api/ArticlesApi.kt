7488c0b0edda0d4d309b7fd0dd19e66d8e19ee4f
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-30 19:09:41 +0300

    modify articles api

--- data-remote/src/main/java/com/sammy/data_remote/api/ArticlesApi.kt
+++ data-remote/src/main/java/com/sammy/data_remote/api/ArticlesApi.kt
@@ -1,5 +1,14 @@
 package com.sammy.data_remote.api
 
-interface ArticlesApi {
+import com.sammy.data_remote.model.ArticleData
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface ArticlesApi {
+    @GET("search")
+    fun getDna(
+        @Query("q") title: String
+    ): Flow<ArticleData>
 }
