6a1a850537d9f95a9ebf17ce3544566632b30a87
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-30 20:50:49 +0300

    create main activity state view handler'

---
+++ app/src/main/java/com/example/kotlinstarter/models/states/MainActivityArticleViewState.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinstarter.models.states
+
+import com.example.kotlinstarter.models.DocPresenter
+
+data class MainActivityArticleViewState(
+    val isLoading: Boolean,
+    val error: Error?,
+    val locationResults: List<DocPresenter>?
+)
