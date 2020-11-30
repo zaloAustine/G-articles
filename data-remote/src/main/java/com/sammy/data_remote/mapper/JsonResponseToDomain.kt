f10f762cbaea9ce2878246cae382f05ff5d72a22
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-30 19:21:29 +0300

    add a doc to domain extension function mapper

--- data-remote/src/main/java/com/sammy/data_remote/mapper/JsonResponseToDomain.kt
+++ data-remote/src/main/java/com/sammy/data_remote/mapper/JsonResponseToDomain.kt
@@ -1,2 +1,17 @@
 package com.sammy.data_remote.mapper
 
+import com.sammy.data_remote.model.Doc
+import com.sammy.domain.model.DocDomain
+
+internal fun Doc.toDomain():DocDomain =
+    DocDomain(
+        this.abstract,
+        this.article_type,
+        this.author_display,
+        this.eissn,
+        this.id,
+        this.journal,
+        this.publication_date,
+        this.score,
+        this.title_display
+    )
